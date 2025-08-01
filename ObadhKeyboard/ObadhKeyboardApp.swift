ba7e8d83c8979085ed94c04068724851a4bef609
yeasar <syeasar.kuet@gmail.com>
2025-08-01 21:48:07 +0600

    Initial Commit

---
+++ ObadhKeyboard/ObadhKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObadhKeyboardApp.swift
+//  ObadhKeyboard
+//
+//  Created by yeasar on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ObadhKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObadhKeyboardTests/ObadhKeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObadhKeyboardTests.swift
+//  ObadhKeyboardTests
+//
+//  Created by yeasar on 1/8/25.
+//
+
+import Testing
+@testable import ObadhKeyboard
+
+struct ObadhKeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
