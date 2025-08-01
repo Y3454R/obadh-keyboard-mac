74e6e452e899021f95ebc0db5595f97188744b17
yeasar <syeasar.kuet@gmail.com>
2025-08-01 22:34:45 +0600

    :ghost: chores: Add module and obadh_ffi header

---
+++ ObadhEngineBridge/obadh_ffi.h
@@ -0,0 +1,14 @@
+//
+//  obadh_ffi.h
+//  ObadhKeyboard
+//
+//  Created by yeasar on 1/8/25.
+//
+
+#ifndef obadh_ffi_h
+#define obadh_ffi_h
+
+const char *transliterate(const char *input);
+void free_string(const char *s);
+
+#endif /* obadh_ffi_h */
